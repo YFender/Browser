fafaec9f13f93e3652a12286e0abaab399d4f5ce
YFen <yurik2159@inbox.ru>
2022-03-25 17:02:14 +0300

    h

--- main.py
+++ main.py
@@ -12,13 +12,13 @@ class Browser(QtWidgets.QMainWindow):
         self.ui = Ui_Browser()
         self.ui.setupUi(self)
 
-        self.ui.toolButton_back.clicked.connect(self.back)
-        self.ui.toolButton_home.clicked.connect(self.home)
-        self.ui.toolButton_search.clicked.connect(self.search)
-        self.ui.toolButton_refresh.clicked.connect(self.refresh)
-
         self.webview = QWebEngineView()
         self.ui.gridLayout.addWidget(self.webview, 1, 0, 1, 7)
+
+        self.ui.toolButton_back.clicked.connect(self.webview.back)
+        self.ui.toolButton_home.clicked.connect(self.home)
+        self.ui.toolButton_search.clicked.connect(self.search)
+        self.ui.toolButton_refresh.clicked.connect(self.webview.reload)
 
         self.ui.toolButton_back.setToolTip("Назад")
         self.ui.toolButton_refresh.setToolTip("Обновить страницу")
@@ -30,38 +30,34 @@ class Browser(QtWidgets.QMainWindow):
 
         self.home()
 
-    def back(self):
-        self.webview.back()
-
-    def refresh(self):
-        self.webview.reload()
-
     def home(self):
         home_page = QtCore.QUrl("https://google.com")
         self.webview.load(home_page)
 
     def search(self):
-        # print(self.webview.page().url())
         search_text = self.ui.lineEdit.text()
+        print(match(
+            '^((ftp|http|https):\/\/)?(www\.)?([A-Za-zА-Яа-я0-9]{1}[A-Za-zА-Яа-я0-9\-]*\.?)*\.{1}[A-Za-zА-Яа-я0-9-]{2,8}(\/([\w#!:.?+=&%@!\-\/])*)?', search_text))
         if search_text != "":
-            # print(match(
-            #     '^((ftp|http|https):\/\/)?(www\.)?([A-Za-zА-Яа-я0-9]{1}[A-Za-zА-Яа-я0-9\-]*\.?)*\.{1}[A-Za-zА-Яа-я0-9-]{2,8}(\/([\w#!:.?+=&%@!\-\/])*)?', search_text))
             if not match('^((ftp|http|https):\/\/)?(www\.)?([A-Za-zА-Яа-я0-9]{1}[A-Za-zА-Яа-я0-9\-]*\.?)*\.{1}[A-Za-zА-Яа-я0-9-]{2,8}(\/([\w#!:.?+=&%@!\-\/])*)?', search_text):
                 search_text = QtCore.QUrl(
                     "https://google.com/search?q=" + search_text)
-                self.webview.load(search_text)
-                # print(self.webview.page().url())
+                try:
+                    self.webview.load(search_text)
+                except:
+                    print("huy")
             else:
                 if not search_text.startswith("http"):
                     search_text = QtCore.QUrl("http://" + search_text)
                     self.webview.load(search_text)
-                    # self.ui.lineEdit.setText(
-                    #     "http://" + self.ui.lineEdit.text())
-                    # print(self.webview.page().url())
+                else:
+                    search_text = QtCore.QUrl(search_text)
+                    self.webview.load(search_text)
 
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(argv)
     myapp = Browser()
     myapp.show()
+    exit(app.exec_())
     exit(app.exec_())
